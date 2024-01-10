746a4b92f72b442299a5fae89076374545bd8a51
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 04:57:14 +0700

    add suspend fun deleteKontak, val kontakId, val uiState in DetailViewModel

--- app/src/main/java/com/example/pam_final/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/pam_final/ui/detail/DetailViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pam_final.data.KontakRepository
+import com.example.pam_final.ui.DetailUIState
+import com.example.pam_final.ui.toDetailKontak
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
@@ -15,5 +20,22 @@ class DetailViewModel (
         private const val TIMEOUT_MILLIS = 5_000L
     }
 
+    val kontakId: String = checkNotNull(savedStateHandle[DetailDestination.kontakId])
+
+    val uiState: StateFlow<DetailUIState> =
+        repository.getKontakById(kontakId)
+            .filterNotNull()
+            .map {
+                DetailUIState(addEvent = it.toDetailKontak())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIState()
+            )
+
+    suspend fun deleteKontak() {
+        repository.delete(kontakId)
+
+    }
 
 }
