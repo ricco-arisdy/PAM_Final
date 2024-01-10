b1d9ac52e0ba1f21adb3251bc7580019321dad81
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 04:13:49 +0700

    add BodyHome, Scaffold in fun HomeScreen

--- app/src/main/java/com/example/pam_final/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/home/HomeScreen.kt
@@ -5,36 +5,45 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam_final.R
 import com.example.pam_final.model.Kontak
 import com.example.pam_final.navigation.DestinasiNavigasi
-
+import com.example.pam_final.ui.KontakTopAppBar
+import com.example.pam_final.ui.PenyediaViewModel
 
 
 object DestinasiHome : DestinasiNavigasi {
@@ -42,6 +51,54 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = "Anggota"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            KontakTopAppBar(
+                title = "Anggota",
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        },
+    ) { innerPadding ->
+        val uiStateKontak by viewModel.homeUiState.collectAsState()
+
+
+
+        BodyHome(
+            itemKontak = uiStateKontak.listKontak,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onSiswaClick = onDetailClick,
+            onSearchQueryChanged = { query: String ->
+                viewModel.setSearchQuery(query)
+            }
+        )
+    }
+}
 
 
 @Composable
