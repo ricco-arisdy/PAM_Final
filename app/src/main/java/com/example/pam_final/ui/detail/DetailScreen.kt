b9eb612a7992ea0bd0e162b6c62b17f5f97848ac
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 04:34:08 +0700

    add Row, private fun ItemDetailsRow in DetailScreen

--- app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
@@ -1,10 +1,13 @@
 package com.example.pam_final.ui.detail
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.example.pam_final.navigation.DestinasiNavigasi
 
 object DetailDestination : DestinasiNavigasi {
@@ -12,6 +15,17 @@ object DetailDestination : DestinasiNavigasi {
     override val titleRes = "Detail Kontak"
     const val kontakId = "itemId"
     val routeWithArgs = "$route/{$kontakId}"
+}
+
+@Composable
+private fun ItemDetailsRow(
+    labelResID: String, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = labelResID, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
 }
 
 @Composable
