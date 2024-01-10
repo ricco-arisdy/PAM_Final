c068158909315292b99e89159d2884a90b188016
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-10 23:59:14 +0700

    add fungsion forminput to AddScreen

--- app/src/main/java/com/example/pam_final/ui/add/AddScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/add/AddScreen.kt
@@ -1,2 +1,54 @@
 package com.example.pam_final.ui.add
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.example.pam_final.R
+import com.example.pam_final.ui.DetailKontak
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    detailKontak: DetailKontak,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailKontak) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailKontak.nama,
+            onValueChange = {onValueChange(detailKontak.copy(nama=it)) },
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailKontak.alamat,
+            onValueChange = {onValueChange(detailKontak.copy(alamat=it)) },
+            label = { Text(stringResource(R.string.alamat)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailKontak.telpon,
+            onValueChange = {onValueChange(detailKontak.copy(telpon=it)) },
+            label = { Text(stringResource(R.string.telpon)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+    }
+}
