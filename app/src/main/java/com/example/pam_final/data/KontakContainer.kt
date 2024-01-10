367eb960f2e12ea23fb64e844e06936f90b1f81d
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-10 23:07:33 +0700

    add KontakRepository class

--- app/src/main/java/com/example/pam_final/data/KontakContainer.kt
+++ app/src/main/java/com/example/pam_final/data/KontakContainer.kt
@@ -20,3 +20,50 @@ interface KontakRepository{
     suspend fun delete(kontakId: String)
     fun getKontakById(kontakId: String): Flow<Kontak>
 }
+
+class KontakRepositoryImpl(private val firestore: FirebaseFirestore):KontakRepository{
+    override fun getAllSiswaStream(): Flow<List<Kontak>> = flow{
+        val snapshot = firestore.collection("Kontak")
+            .orderBy("nama", Query.Direction.ASCENDING)
+            .get()
+            .await()
+        val kontak = snapshot.toObjects(Kontak::class.java)
+        emit(kontak)
+    }.flowOn(Dispatchers.IO)
+
+    override fun getSiswaStream(id: Int): Flow<Kontak?> {
+        TODO("Not yet implemented")
+    }
+
+
+
+    override suspend fun save(kontak: Kontak): String {
+        return try {
+            val documentReference = firestore.collection("Kontak").add(kontak).await()
+
+            firestore.collection("Kontak").document(documentReference.id)
+                .set(kontak.copy(id = documentReference.id))
+            "Berhasil + ${documentReference.id}"
+        } catch (e: Exception){
+            Log.w(ContentValues.TAG,"Error adding document",e)
+            "Gagal $e"
+        }
+    }
+
+    override suspend fun update(kontak: Kontak) {
+        firestore.collection("Kontak").document(kontak.id).set(kontak).await()
+    }
+
+    override suspend fun delete(kontakId: String) {
+        firestore.collection("Kontak").document(kontakId).delete().await()
+    }
+
+    override fun getKontakById(kontakId: String): Flow<Kontak> {
+        return flow {
+            val snapshot = firestore.collection("Kontak").document(kontakId).get().await()
+            val kontak = snapshot.toObject(Kontak::class.java)
+            emit(kontak!!)
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
