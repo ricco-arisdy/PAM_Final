2fcf3f8b877ee5d8af15139c5fa0d465446fad8a
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-10 23:23:15 +0700

    add file screen, and kontakapp

---
+++ app/src/main/java/com/example/pam_final/ui/KontakApp.kt
@@ -0,0 +1,31 @@
+package com.example.pam_final.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KontakApp(
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pam_final/ui/add/AddScreen.kt
@@ -0,0 +1,2 @@
+package com.example.pam_final.ui.add
+
