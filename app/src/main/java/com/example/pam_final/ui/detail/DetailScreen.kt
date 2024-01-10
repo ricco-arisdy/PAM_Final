ebfbf37b96336e32714185e4e8dcac228b540f57
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 05:05:19 +0700

    add ItemDetailsBody, FloatingActionButton, KontakTopAppBar, Scaffold in DetailScreen

--- app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
@@ -6,31 +6,92 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam_final.model.Kontak
 import com.example.pam_final.navigation.DestinasiNavigasi
 import com.example.pam_final.ui.DetailUIState
+import com.example.pam_final.ui.KontakTopAppBar
+import com.example.pam_final.ui.PenyediaViewModel
 import com.example.pam_final.ui.toKontak
+import kotlinx.coroutines.launch
 
 object DetailDestination : DestinasiNavigasi {
     override val route = "item_details"
     override val titleRes = "Detail Kontak"
     const val kontakId = "itemId"
     val routeWithArgs = "$route/{$kontakId}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateToEditItem: (String) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            KontakTopAppBar(
+                title = DetailDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEditItem(uiState.value.addEvent.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "",
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        ItemDetailsBody(
+            detailUIState = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+
+            )
+    }
 }
 
 @Composable
