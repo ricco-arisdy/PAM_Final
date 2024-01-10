aa84a0298ecf52d4b78770bb7e9769af9a50411c
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 04:29:10 +0700

    add object DetailDestination,private fun DeleteConfirmationDialog, in DetailScreen

--- app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/detail/DetailScreen.kt
@@ -1,2 +1,35 @@
 package com.example.pam_final.ui.detail
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pam_final.navigation.DestinasiNavigasi
+
+object DetailDestination : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = "Detail Kontak"
+    const val kontakId = "itemId"
+    val routeWithArgs = "$route/{$kontakId}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Are you sure") },
+        text = { Text("Delete") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "No")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
