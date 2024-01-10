2d0fbcc80b161133280e644c6bdbba8bb939e51f
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-10 23:47:29 +0700

    add fungsion detailkontak and data class DetailUIState

--- app/src/main/java/com/example/pam_final/ui/UIState.kt
+++ app/src/main/java/com/example/pam_final/ui/UIState.kt
@@ -1,5 +1,7 @@
 package com.example.pam_final.ui
 
+import com.example.pam_final.model.Kontak
+
 data class UIStateKontak(
     val detailKontak: DetailKontak= DetailKontak(),
 )
@@ -9,4 +11,16 @@ data class  DetailKontak(
     val nama : String= "",
     val alamat : String= "",
     val telpon : String= "",
+)
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya*/
+fun DetailKontak.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon
+)
+
+data class DetailUIState(
+    val addEvent: DetailKontak = DetailKontak(),
 )
