2ec3456bcf5250107f9ec6b7a44c7ab9e318550e
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 20:07:37 +0700

    add padding in SearchBar

--- app/src/main/java/com/example/pam_final/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/home/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -241,5 +243,8 @@ fun SearchBar(
             }
         },
         modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        textStyle = LocalTextStyle.current.copy(color = Color.Black),
     )
 }
