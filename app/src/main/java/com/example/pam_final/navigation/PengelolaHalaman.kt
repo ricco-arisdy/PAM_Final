04b53f3502ff3548ac5764c51a575382f9f4cebb
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 12:16:20 +0700

    add composable DestinasiHome, Kontak, Detail, edit, in PengelolaHalaman

--- app/src/main/java/com/example/pam_final/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_final/navigation/PengelolaHalaman.kt
@@ -1,8 +1,78 @@
 package com.example.pam_final.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pam_final.ui.add.AddScreen
+import com.example.pam_final.ui.add.DestinasiKontak
+import com.example.pam_final.ui.detail.DetailDestination
+import com.example.pam_final.ui.detail.DetailScreen
+import com.example.pam_final.ui.edit.EditDestination
+import com.example.pam_final.ui.edit.EditScreen
+import com.example.pam_final.ui.home.DestinasiHome
+import com.example.pam_final.ui.home.HomeScreen
 
 @Composable
-fun PengelolaHalaman(){
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(
+            DestinasiHome.route
+        ) {
+            HomeScreen(navigateToItemEntry = {
+                navController.navigate(DestinasiKontak.route)
+            },
+                onDetailClick = { itemId ->
+                    navController.navigate("${DetailDestination.route}/$itemId")
+                    println("itemId: $itemId")
+                })
+        }
+        composable(DestinasiKontak.route) {
+            AddScreen(navigateBack = {
+                navController.popBackStack()
+            })
 
+        }
+
+        composable(
+            route = DetailDestination.routeWithArgs,
+            arguments = listOf(navArgument(DetailDestination.kontakId) {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            val kontakId = backStackEntry.arguments?.getString(DetailDestination.kontakId)
+            kontakId?.let {
+                DetailScreen(
+                    navigateBack = { navController.popBackStack() },
+                    navigateToEditItem = {
+                        navController.navigate("${EditDestination.route}/$kontakId")
+                        println("kontakId: $kontakId")
+                    }
+                )
+            }
+        }
+
+        composable(
+            route = EditDestination.routeWithArgs,
+            arguments = listOf(navArgument(EditDestination.kontakId) {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            val kontakId = backStackEntry.arguments?.getString(EditDestination.kontakId)
+            kontakId?.let {
+                EditScreen(
+                    navigateBack = { navController.popBackStack() },
+                    onNavigateUp = { navController.navigateUp() }
+                )
+            }
+        }
+    }
 }
