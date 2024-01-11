50d3f4606ad0758f5f3b1f062fd0f1aa30ce2c1e
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 11:47:03 +0700

    add AddKontakBody, Scaffold, fun EditScreen in EditScreen

--- app/src/main/java/com/example/pam_final/ui/edit/EditScreen.kt
+++ app/src/main/java/com/example/pam_final/ui/edit/EditScreen.kt
@@ -1,10 +1,60 @@
 package com.example.pam_final.ui.edit
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam_final.navigation.DestinasiNavigasi
+import com.example.pam_final.ui.KontakTopAppBar
+import com.example.pam_final.ui.PenyediaViewModel
+import com.example.pam_final.ui.add.AddKontakBody
+import kotlinx.coroutines.launch
 
 object EditDestination : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes ="Edit Kontak"
     const val kontakId = "itemId"
     val routeWithArgs = "${EditDestination.route}/{$kontakId}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            KontakTopAppBar(
+                title =EditDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        AddKontakBody(
+            uiStateKontak = viewModel.kontakUiState,
+            onKontakValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
