7f2dee43739f218791b2723c1934bacae82fb91e
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-01-11 11:22:04 +0700

    add class EditViewModel in EditViewModel

---
+++ app/src/main/java/com/example/pam_final/ui/edit/EditViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.pam_final.ui.edit
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pam_final.data.KontakRepository
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: KontakRepository
+): ViewModel() {
+}
